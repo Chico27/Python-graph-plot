d4b7537c654fb11c1e496f226b07f7007d7043a2
Chico27 <masamasa.987654321@gmail.com>
2019-05-28 20:20:44 +0900

    new

--- main.py
+++ main.py
@@ -1,17 +1,39 @@
-# import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+sns.set()
+sns.set_style('whitegrid')
+sns.set_palette('gray')
 
 myFile = open("data.txt")
 
 line = myFile.readline()
+x = []
+y = []
 
 while line:
-    print(line)
-    line = myFile.readline()
     data = line.strip().split(",")
     print(data)
-    # x = data[0]
-    # y = data[1]
-    # print(x)
+    print(data[0])
+    print(data[1])
+    x.append(int(data[0]))
+    y.append(int(data[1]))
+    line = myFile.readline()
+
 
 myFile.close()
+print(x)
+print(y)
+outputx = np.array(x)
+outputy = np.array(y)
 
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+ax.plot(outputx, outputy)
+ax.set_xlabel('cold treatment [hour]')
+ax.set_ylabel('gene activity')
+ax.set_ylim(0, 2)
+plt.show()
